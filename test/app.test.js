e02d2e0f07176f1313d87f85a4bdd51bd462d6df
Leslie Shea <leslieyshea@gmail.com>
2019-04-15 16:38:43 -0700

    create profile test passing

---
+++ test/app.test.js
@@ -0,0 +1,26 @@
+const request = require('supertest');
+const app = require('../lib/app');
+const Profile = require('../lib/models/Profile');
+
+describe('Profile routes', () => {
+  afterEach(() => {
+    return Profile.drop();
+  });
+  it('creates a new profile', () => {
+    return request(app)
+      .post('/profile')
+      .send({
+        name: 'cindy',
+        character: 'bender'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          name: 'cindy',
+          character: 'bender',
+          _id: expect.any(String),
+          tagline: expect.any(String)
+        });
+      });
+  });
+});
+
