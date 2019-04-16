e4af659f63b2c4bb35f4d159ade115cab50d6099
Leslie Shea <leslieyshea@gmail.com>
2019-04-15 17:13:39 -0700

    fixed mock tests and fns

--- test/app.test.js
+++ test/app.test.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const Profile = require('../lib/models/Profile');
 
+jest.mock('../lib/services/futurama-api.js');
 
 describe('Profile routes', () => {
   afterEach(() => {
--- test/services/futurama-api.test.js
+++ test/services/futurama-api.test.js
@@ -5,12 +5,8 @@ jest.mock('../../lib/services/futurama-api.js');
 describe('futurama api', () => {
   it('can get a quote by character', () => {
     return getCharacterQuote('Bender')
-      .then(character => {
-        expect(character).toEqual({
-          character: 'Bender',
-          quote: 'hello there',
-          image: 'fakeimg.png'
-        });
+      .then(tagline => {
+        expect(tagline).toEqual('hello');
       });
   });
 });
